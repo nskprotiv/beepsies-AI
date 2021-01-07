4ddbbfa4bca5fb5b4967e23fd8934b81b1d3694c
nskprotiv <59990012+nskprotiv@users.noreply.github.com>
2021-01-07 15:05:42 +1000

    Add files via upload

---
+++ BeepsiesLauncher.ps1
@@ -0,0 +1,11 @@
+cls
+Write-Output("Welcome to Beepsies Launcher")
+Write-Output("Hello! Type down bellow 'Beta'")
+Write-Output("To open Beepsies!")
+$Version = Read-Host -Prompt 'Input Version number'
+$Date = Get-Date
+Write-Host "You opened Beepsies v'$Version' on '$Date'"
+Write-Output("Beepsies can't wait to see you!")
+Write-Output("Loading Beepsies...")
+Start-Sleep(3)
+C:\Users\*\Desktop\Beepsies.py
